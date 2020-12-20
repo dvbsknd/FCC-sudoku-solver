41c0754d1155f07f4d82d701103129932d95852b
Dave Baskind <dave@picketstudio.com>
2020-12-21 10:19:59 +1100

    Roughly implement routing

--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,18 @@ import SudokuSolver from '../controllers/sudoku-solver';
 module.exports = function (app) {
   const solver = new SudokuSolver();
 
+  app.use((req, res, next) => {
+    console.log('[API]', req.method, req.body);
+    next();
+  });
+
   app.route('/api/check')
     .post((req, res) => {
-
     });
 
   app.route('/api/solve')
     .post((req, res) => {
-
+      const { puzzle } = req.body;
+      res.json({ solution: solver.solve(puzzle) });
     });
 };
